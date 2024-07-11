c9396d983091ed1f2014dc714ec077615ef47edd
Rafael Delgado <rafael@delgado.com>
2024-07-11 16:46:25 -0700

    transfer razor pages from SPA project

--- CAPSquadron_WebServer/Program.cs
+++ CAPSquadron_WebServer/Program.cs
@@ -1,8 +1,20 @@
+using CAPSquadron_WebServer.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<IFlightService, FlightService>();
+builder.Services.AddScoped<ICadetTrackerService, CadetTrackerService>();
+
+builder.Services.AddScoped(sp =>
+{
+    var webAPIBaseAddress = "http://localhost:5203/";
+    var httpClient = new HttpClient { BaseAddress = new Uri(webAPIBaseAddress) };
+    return new ApiClient("http://localhost:5203/", httpClient); // Change the base URL as necessary
+});
 
 var app = builder.Build();
 
---
+++ CAPSquadron_WebServer/Services/ICadetTrackerService.cs
@@ -0,0 +1,7 @@
+﻿namespace CAPSquadron_WebServer.Services;
+
+public interface ICadetTrackerService
+{
+    Task<IEnumerable<PersonalCadetTrackerDto>> GetCadetTrackerByCapidsAsync(IEnumerable<int> capids);
+    Task<IEnumerable<PersonalCadetTrackerDto>> GetCadetTrackerAsync();
+}
