444f1bf83b88d7517f4cde934a403b655fd43c52
Rafael Delgado <rafael@delgado.com>
2024-07-12 23:36:57 -0700

    update connection string to use env

--- CAPSquadron_ETL/database.py
+++ CAPSquadron_ETL/database.py
@@ -1,9 +1,10 @@
 # database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://cawg-sq144-server-admin:rafael.delgado.3@192.168.10.170:5432/cawg-sq144-database"
+DATABASE_URL = os.getenv('CAPSQ144_CONNECTIONSTRING_PG')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
