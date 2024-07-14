f065d6189302aaf5f505f34af380b4737c75e3fa
Rafael Delgado <rafael@delgado.com>
2024-07-13 17:10:48 -0700

    add MemberAttributesDto

---
+++ CAPSquadron_API/Models/MemberAttributesDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CAPSquadron_API.Models
+{
+    public class MemberAttributesDto
+    {
+        public int CAPID { get; set; }
+        public string? Name { get; set; }
+        public bool HasWrightBrothersAchievement { get; set; }
+        public bool HasGESCertification { get; set; }
+        public bool HasCurryAchievement { get; set; }
+        public bool HasCawgcapEmail { get; set; }
+    }
+}
