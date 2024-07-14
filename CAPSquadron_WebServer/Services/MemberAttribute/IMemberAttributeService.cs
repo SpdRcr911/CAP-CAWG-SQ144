058400b21e3da4f6ef819372fcf376f2f8cd31f4
Rafael Delgado <rafael@delgado.com>
2024-07-14 09:25:33 -0700

    add member attributes service

---
+++ CAPSquadron_WebServer/Services/MemberAttribute/IMemberAttributeService.cs
@@ -0,0 +1,7 @@
+﻿namespace CAPSquadron_WebServer.Services.MemberAttribute;
+
+public interface IMemberAttributeService
+{
+    Task<IEnumerable<MemberAttributesDto>> GetMemberAttributesAsync();
+    Task<MemberAttributesDto> GetMemberAttributesByCapIdAsync(int capid);
+}
