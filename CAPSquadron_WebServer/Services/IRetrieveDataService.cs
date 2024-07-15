7f876815eeff15408d0da986e9910d870ebcc4d7
Rafael Delgado <rafael@delgado.com>
2024-07-15 11:26:11 -0700

    update web project to use the member instead of attendee sing ins

---
+++ CAPSquadron_WebServer/Services/IRetrieveDataService.cs
@@ -0,0 +1,8 @@
+﻿namespace CAPSquadron_WebServer.Services;
+
+public interface IRetrieveDataService<T> where T : class
+{
+    Task<IEnumerable<T>> GetAsync();
+    Task<T> GetAsync(int id);
+    Task<IEnumerable<T>> GetAsync(IEnumerable<int> ids);
+}
