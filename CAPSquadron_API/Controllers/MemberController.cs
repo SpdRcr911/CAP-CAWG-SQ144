f6fe51ee6dbc7926de15fd724406dd3db99a3642
Rafael Delgado <rafael@delgado.com>
2024-07-15 11:24:30 -0700

    update member controller
    Clean up the names

--- CAPSquadron_API/Controllers/MemberController.cs
+++ CAPSquadron_API/Controllers/MemberController.cs
@@ -6,37 +6,37 @@ namespace CAPSquadron_API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class MembershipController(ICsvParsingService csvParsingService, IRetrieveDataService<Member> retrieveDataService, IProcessDataService<MemberCsv> processDataService) : ControllerBase
+public class MemberController(ICsvParsingService csvParsingService, IRetrieveDataService<Member> retrieveDataService, IProcessDataService<MemberCsv> processDataService) : ControllerBase
 {
-    [HttpGet(Name = nameof(GetMembershipIns))]
+    [HttpGet(Name = nameof(GetMembers))]
     [ProducesResponseType<List<Member>>(StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetMembershipIns()
+    public async Task<IActionResult> GetMembers()
     {
         var membership = await retrieveDataService.GetAsync();
         return Ok(membership);
     }
 
-    [HttpGet("{capid}", Name = nameof(GetMembershipIn))]
+    [HttpGet("{capid}", Name = nameof(GetMember))]
     [ProducesResponseType<Member>(StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetMembershipIn(int capid)
+    public async Task<IActionResult> GetMember(int capid)
     {
         var membership = await retrieveDataService.GetAsync(capid);
         return Ok(membership);
     }
 
-    [HttpPost("capids", Name = nameof(GetMembershipInsByCapids))]
+    [HttpPost("capids", Name = nameof(GetMembersByCapids))]
     [ProducesResponseType<IEnumerable<Member>>(StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetMembershipInsByCapids([FromBody] IEnumerable<int> capids)
+    public async Task<IActionResult> GetMembersByCapids([FromBody] IEnumerable<int> capids)
     {
         var membership = await retrieveDataService.GetAsync(capids);
         return Ok(membership);
     }
 
 
-    [HttpPost("upload", Name = nameof(UploadMembershipList))]
+    [HttpPost("upload", Name = nameof(UploadMembersList))]
     [Consumes("multipart/form-data")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<IActionResult> UploadMembershipList(IFormFile file)
+    public async Task<IActionResult> UploadMembersList(IFormFile file)
     {
         if (file == null || file.Length == 0)
             return BadRequest("No file uploaded.");
