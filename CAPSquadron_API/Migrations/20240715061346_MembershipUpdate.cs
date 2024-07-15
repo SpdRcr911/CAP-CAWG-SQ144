27e89dfcc9af1040b6bca6749987ff62920cba1f
Rafael Delgado <rafael@delgado.com>
2024-07-15 07:06:37 -0700

    add membership migration

---
+++ CAPSquadron_API/Migrations/20240715061346_MembershipUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CAPSquadron_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MembershipUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "gender1",
+                table: "members",
+                newName: "gender");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "gender",
+                table: "members",
+                newName: "gender1");
+        }
+    }
+}
