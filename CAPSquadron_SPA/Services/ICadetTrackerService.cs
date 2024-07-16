5517a5f6576aa1ae8f97c2f6cfbe80f39dd3e7ca
Rafael Delgado <rafael@delgado.com>
2024-07-15 19:40:26 -0700

    remove SPA project

--- CAPSquadron_SPA/Services/ICadetTrackerService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CAPSquadron_SPA.Services;
-
-public interface ICadetTrackerService
-{
-    Task<IEnumerable<PersonalCadetTrackerDto>> GetCadetTrackerByCapidsAsync(IEnumerable<int> capids);
-    Task<IEnumerable<PersonalCadetTrackerDto>> GetCadetTrackerAsync();
-}
--- CAPSquadron_SPA/Services/IFlightService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CAPSquadron_SPA.Services;
-
-public interface IFlightService
-{
-    Task<IEnumerable<FlightDto>> GetFlightsAsync();
-    Task<FlightDto> GetFlightByIdAsync(int id);
-}
