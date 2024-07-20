1466d278973c0a5d05aa1c624c036732487d24ee
Rafael Delgado <rafael@delgado.com>
2024-07-19 22:33:29 -0700

    create controller

---
+++ CAPSquadron_API/Controllers/AttendanceReportController.cs
@@ -0,0 +1,63 @@
+﻿using CAPSquadron_API.Models;
+using CAPSquadron_API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAPSquadron_API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AttendanceReportController(IAttendanceReportService attendanceReportService, IProcessDataService<AttendanceReportCsv> processDataService, ICsvParsingService csvParsingService) : ControllerBase
+{
+
+    [HttpGet("get-dates", Name = nameof(GetAttendanceReportDates))]
+    [ProducesResponseType<IEnumerable<DateTimeOffset?>>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAttendanceReportDates()
+    {
+        var attendanceData = await attendanceReportService.GetAttendanceReportDatesAsync();
+        return Ok(attendanceData);
+    }
+
+    [HttpGet("record/{capid}", Name = nameof(GetAttendanceReportForMember))]
+    [ProducesResponseType<IEnumerable<AttendanceReport>>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAttendanceReportForMember(int capid)
+    {
+        var attendanceData = await attendanceReportService.GetAttendanceReportForMemberAsync(capid);
+        return Ok(attendanceData);
+    }
+
+    [HttpGet(Name = nameof(GetAllAttendiesByDate))]
+    [ProducesResponseType<IEnumerable<AttendanceReport>>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllAttendiesByDate(DateTimeOffset date, bool? present)
+    {
+        var attendanceData = await attendanceReportService.GetAllAttendiesByDateAsync(date, present);
+        return Ok(attendanceData);
+    }
+
+    [HttpGet("get-capids/{date}",Name = nameof(GetAttendiesCapIdsByDate))]
+    [ProducesResponseType<IEnumerable<int>>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAttendiesCapIdsByDate(DateTimeOffset date)
+    {
+        var attendanceData = await attendanceReportService.GetAttendiesCapIdsByDateAsync(date);
+        return Ok(attendanceData);
+    }
+
+    [HttpPost("upload", Name = nameof(UploadAttendanceReport))]
+    [Consumes("multipart/form-data")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> UploadAttendanceReport(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            return BadRequest("No file uploaded.");
+
+        try
+        {
+            var attendanceReportCsv = csvParsingService.ParseCsv<AttendanceReportCsv>(file.OpenReadStream());
+            await processDataService.ProcessAsync(attendanceReportCsv);
+            return Ok("Membership list updated successfully.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message} ");
+        }
+    }
+}
