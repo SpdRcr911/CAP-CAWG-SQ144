65c8391f17a243aeed746d3c35d5101b8ac12558
Rafael Delgado <rafael@delgado.com>
2024-07-22 22:13:15 -0700

    add models for CadetPhysicalFitnessTrainingReport

---
+++ CAPSquadron_API/Models/CadetPhysicalFitnessTrainingReport.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace CAPSquadron_API.Models;
+
+public class CadetPhysicalFitnessTrainingReport
+{
+    public int Id { get; set; }
+    [Required]
+    public int CAPID { get; set; }
+    public string? FullName { get; set; }
+    public string? Rank { get; set; }
+    public string? HFZCred { get; set; }
+    public int PacerRunReq { get; set; }
+    public TimeSpan MileRunReq { get; set; }
+    public int CurlUpReq { get; set; }
+    public int PushUpReq { get; set; }
+    public int SitAndReachReq { get; set; }
+    public DateOnly? Expiration { get; set; }
+    public DateTimeOffset RecordTimeStamp { get; set; }
+}
---
+++ CAPSquadron_API/Models/CadetPhysicalFitnessTrainingReportCsv.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CAPSquadron_API.Models;
+
+public class CadetPhysicalFitnessTrainingReportCsv
+{
+    [Column("HFZ Cred")]
+    public string? HFZCred { get; set; }
+    [Column("Full Name")]
+    public string? FullName { get; set; }
+    [Column("Pacer Run Req.")]
+    public int PacerRunReq { get; set; }
+    [Column("Mile Run Req.")]
+    public string? MileRunReq { get; set; }
+    [Column("Curl Up Req.")]
+    public int CurlUpReq { get; set; }
+    [Column("Push Up Req.")]
+    public int PushUpReq { get; set; }
+    [Column("Sit & Reach Req")]
+    public int SitAndReachReq { get; set; }
+    public string? Expiration { get; set; }
+}
