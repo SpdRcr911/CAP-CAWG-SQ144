1a3336efeed23d5c8562774a2542ba5c0c97a48a
Rafael Delgado <rafael@delgado.com>
2024-08-11 18:08:49 -0700

    add cade session to cadet website

--- CAPSquadron_Cadet/Program.cs
+++ CAPSquadron_Cadet/Program.cs
@@ -1,11 +1,16 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using BlazorBootstrap; // Add this line
+using CAPSquadron_Shared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddBlazorBootstrap(); // Add this line
+builder.Services.AddBlazorBootstrap();
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped(sp => new ApiClient(Environment.GetEnvironmentVariable("ApiClientBaseUrl"), sp.GetRequiredService<HttpClient>()));
+builder.Services.AddSession();
+
+builder.Services.AddScoped<IValidateCadet, ValidateCadet>();
+
 
 var app = builder.Build();
 
@@ -20,6 +25,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
---
+++ CAPSquadron_Shared/Services/Member/ValidateCadet.cs
@@ -0,0 +1,27 @@
+﻿namespace CAPSquadron_Shared.Services;
+
+public interface IValidateCadet
+{
+    Task<bool> ValidateAsync(int capId, DateOnly joinDate);
+}
+
+public class ValidateCadet(ApiClient apiClient) : IValidateCadet
+{
+    public async Task<bool> ValidateAsync(int capId, DateOnly joinDate)
+    {
+        try
+        {
+            var cadet = await apiClient.GetMemberByCapIdAsync(capId);
+            if (cadet is not null)
+            {
+                return cadet.Joined == joinDate;
+            }
+            return false;
+        }
+        catch (ApiException ex) when (ex.StatusCode == 404)
+        {
+
+            return false;
+        }
+    }
+}
