4f5a1f156a1a515d69b69897f11ae94c228026ed
Rafael Delgado <rafael@delgado.com>
2024-08-11 20:05:34 -0700

    abstract out the Cadet Session parts

---
+++ CAPSquadron_Shared/CadetSessionComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CAPSquadron_Shared;
+
+public class CadetSessionComponent : ComponentBase
+{
+    [Inject] protected IJSRuntime? JSRuntime { get; set; }
+    [Inject] protected NavigationManager? NavigationManager { get; set; }
+
+    protected string? capid;
+    private bool _firstRender = true;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender && _firstRender)
+        {
+            _firstRender = false;
+
+            // Perform JavaScript interop after the component has been rendered
+            capid = await JSRuntime!.InvokeAsync<string>("getSessionStorage", "CAPID");
+
+            if (string.IsNullOrEmpty(capid))
+            {
+                NavigationManager!.NavigateTo("/login");
+            }
+            else
+            {
+                StateHasChanged(); // Ensure the UI is updated if capid is found
+            }
+        }
+    }
+}
