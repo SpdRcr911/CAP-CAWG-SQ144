fa926d06845eea15fb7b8ad923dd859ee630e5d1
Rafael Delgado <rafael@delgado.com>
2024-08-15 19:53:34 -0700

    update cadet report in cadet web portal

--- CAPSquadron_Cadet/Program.cs
+++ CAPSquadron_Cadet/Program.cs
@@ -1,6 +1,9 @@
 using CAPSquadron_Shared.Services;
+using CAPSquadron_Shared.Services.Attendance;
 using CAPSquadron_Shared.Services.CadetTracker;
+using CAPSquadron_Shared.Services.FileHandling;
 using CAPSquadron_Shared.Services.Meeting;
+using CAPSquadron_Shared.Services.MemberAttribute;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
@@ -11,10 +14,20 @@ builder.Services.AddHttpClient();
 builder.Services.AddScoped(sp => new ApiClient(Environment.GetEnvironmentVariable("ApiClientBaseUrl"), sp.GetRequiredService<HttpClient>()));
 builder.Services.AddSession();
 
-builder.Services.AddScoped<IValidateCadet, ValidateCadet>();
+builder.Services.AddScoped<IAttendanceSignInService, AttendanceSignInService>();
+builder.Services.AddScoped<ICadetTrackerService, CadetTrackerService>();
+builder.Services.AddScoped<IMemberAttributeService, MemberAttributeService>();
 builder.Services.AddScoped<IMemberServcie, MemberService>();
 builder.Services.AddScoped<IRetrieveDataService<Member>, MemberService>();
-builder.Services.AddScoped<ICadetTrackerService, CadetTrackerService>();
+builder.Services.AddScoped<IAttendanceService, AttendanceService>();
+
+builder.Services.AddScoped<IFileUploadService, FileUploadService>();
+builder.Services.AddScoped<IFileUploadService, FileUploadService>();
+builder.Services.AddScoped<IFileHandler, AttendanceFileHandler>();
+builder.Services.AddScoped<IFileHandler, CadetTrackerFileHandler>();
+builder.Services.AddScoped<IFileHandlerFactory, FileHandlerFactory>();
+
+builder.Services.AddScoped<IValidateCadet, ValidateCadet>();
 builder.Services.AddScoped<IMeetingService, MeetingService>();
 
 var app = builder.Build();
