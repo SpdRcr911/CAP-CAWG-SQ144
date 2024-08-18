5e0352232e83ea03ed8b787f1cf42a8f83243991
Rafael Delgado <rafael@delgado.com>
2024-08-18 10:46:48 -0700

    update shared meeting service and flightMembership model

---
+++ CAPSquadron_Shared/Models/FlightMembership.cs
@@ -0,0 +1,11 @@
+﻿using CAPSquadron_Shared.Services;
+
+namespace CAPSquadron_Shared.Models;
+
+public class FlightMembership
+{
+    public int FlightId { get; set; }
+    public Member? FlightCommander { get; set; }
+    public List<Member> FlightSergeants { get; set; } = new();
+    public List<Member> Members { get; set; } = new();
+}
