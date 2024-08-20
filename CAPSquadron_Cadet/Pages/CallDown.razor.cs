d4c198f7cda12c64796f1a20e385ac5a7ba2656c
Rafael Delgado <rafael@delgado.com>
2024-08-20 14:33:00 -0700

    add call down component to cadet site

--- CAPSquadron_Cadet/Pages/CallDown.razor.cs
+++ CAPSquadron_Cadet/Pages/CallDown.razor.cs
@@ -1,20 +1,21 @@
 using CAPSquadron_Shared;
 using CAPSquadron_Shared.Services;
 using CAPSquadron_Shared.Models;
+using BlazorBootstrap;
 
 namespace CAPSquadron_Cadet.Pages
 {
     public partial class CallDown : CadetSessionComponentBase
     {
-        private Member? cadet;
-        private PersonalCadetTrackerDto? personalCadetTracker;
-        private MeetingInfoDto? meetingInfo;
-        private CallDownModel formModel = new();
-        private List<RequestItem>? availableRequests;
+        private Member? Cadet;
+        private CallDownModel FormModel = new();
+        private CallDownResponse? CallDownResposne = null;
+
+        private int? CapId { get; set; }
+        private MeetingInfoDto? MeetingInfo { get; set; } = null;
 
         protected override async Task OnInitializedAsync()
         {
-            // Initialize any necessary data here if needed, otherwise remove Task.Delay(1000)
             await base.OnInitializedAsync();
         }
 
@@ -22,40 +23,16 @@ namespace CAPSquadron_Cadet.Pages
         {
             if (!string.IsNullOrEmpty(capid))
             {
-                var capId = Convert.ToInt32(capid);
-                cadet = await MemberService.GetAsync(capId);
-                personalCadetTracker = await CadetTrackerService.GetCadetTrackerByCapidAsync(capId);
-                meetingInfo = await meetingService.GetNextMeeting();
-
-                // Initialize available requests
-                availableRequests = meetingService
-                    .GetAvailableRequestsForCadet(personalCadetTracker)
-                    .Select(r => new RequestItem { Name = r, IsSelected = false })
-                    .ToList(); // Ensure list is stored as a List<T> to keep it stable
+                CapId = Convert.ToInt32(capid);
+                Cadet = await MemberService.GetAsync(CapId.Value);
+                MeetingInfo = await meetingService.GetNextMeeting();
+                CallDownResposne = (await meetingService.GetCallDownsAsync(MeetingInfo!.Date!.Value, CapId)).FirstOrDefault();
             }
         }
 
-        private async Task HandleValidSubmit()
+        protected async Task HandleCallDownUpdated()
         {
-            if (availableRequests is not null && availableRequests.Count != 0)
-            {
-                formModel.Requests = availableRequests
-                    .Where(r => r.IsSelected)
-                    .Select(r => r.Name!)
-                    .ToList() ?? [];
-            }
-
-            var cdr = new CallDownResponse
-            {
-                Attending = formModel.Attending,
-                CapId = Convert.ToInt32(capid),
-                Comments = formModel.Comments,
-                MeetingDate = meetingInfo!.Date!.Value,
-                Reason = formModel.Reason,
-                Requests = formModel.Requests
-            };
-
-            await meetingService.RecordCallDownAsync(cdr);
+            CallDownResposne = (await meetingService.GetCallDownsAsync(MeetingInfo!.Date!.Value, CapId)).FirstOrDefault();
         }
     }
 }
